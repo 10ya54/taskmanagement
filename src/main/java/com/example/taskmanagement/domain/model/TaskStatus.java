6befb3a7516f463662b2403563f9946024728051
10ya54 <840shikamaru@gmail.com>
2025-12-29 14:04:28 +0900

    一覧画面：デザイン修正

--- src/main/java/com/example/taskmanagement/domain/model/TaskStatus.java
+++ src/main/java/com/example/taskmanagement/domain/model/TaskStatus.java
@@ -2,17 +2,19 @@ package com.example.taskmanagement.domain.model;
 
 import lombok.Getter;
 
+
 @Getter
 public enum TaskStatus {
-
-    TODO("未着手"),
-    DOING("進行中"),
-    DONE("完了");
+    TODO("未着手", "bg-secondary"),
+    DOING("進行中", "bg-primary"),
+    DONE("完了", "bg-success");
 
     private final String label;
+    private final String cssClass;
 
-    TaskStatus(String label) {
+    TaskStatus(String label, String cssClass) {
         this.label = label;
+        this.cssClass = cssClass;
     }
 
 }
